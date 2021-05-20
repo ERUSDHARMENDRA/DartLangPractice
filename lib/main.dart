529b036fb9c5b5399811a55647a45dd4aba1cc13
Dharmendra Mehta <dharmendramehta622@gmail.com>
2021-05-20 17:03:43 +0545

    'Exception Handling with the clause provided by dart'

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,48 @@
-void main(){
+void main() {
+
+  print('CASE 1');
+  //CASE 2: When you know the exception to be thrown, use ON Clause
+  try{
+    int result=12~/0;
+    print ('The result is: $result');
+  }on IntegerDivisionByZeroException{
+    print('Can\'t divide by zero');
+  }
+
+  print('CASE 2');
+  //CASE 2: When you don't know the exception to be thrown, use CATCH Clause
+  try{
+    int result=12~/0;
+    print ('The result is: $result');
+  }catch(e){
+    print('The Exception thrown is: $e');
+  }
+
+
+  print('CASE 3');
+  //CASE 2: Using the STACK TRACE to know the events occurred before the Exception was thrown
+  try{
+    int result=12~/0;
+    print ('The result is: $result');
+  }catch(e,s){
+    print('The Exception thrown is: $e');
+    print('STACK TRACE:\n $s');
+  }
+
+  print('CASE 4');
+  //CASE 2: Whether there is an Exception or not, FINALLY Clause is always Executed
+  try{
+    int result=12~/0;
+    print ('The result is: $result');
+  }catch(e,s){
+    print('The Exception thrown is: $e');
+    print('STACK TRACE:\n $s');
+  } finally{
+    print('This is FINALLY Clause and is always executed');
+  }
+
+
+
 
 
 }
